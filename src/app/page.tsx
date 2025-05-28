37c0618b1451ddf990a1a5933ada1272037eac39
Kevin Smith Jimenez Garro <34348998+kevin4809@users.noreply.github.com>
2025-05-28 15:22:38 -0500

    Update url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline/next";
 export default function Home() {
   return (
     <main>
-      <Spline scene='https://prod.spline.design/spSrjF5P3IRaQB10/scene.splinecode' />
+      <Spline scene='https://prod.spline.design/R6sXFove1oIeT0XU/scene.splinecode' />
     </main>
   );
 }
