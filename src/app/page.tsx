644435c02272236255938da4870df72cdbd39f45
Kevin Smith Jimenez Garro <34348998+kevin4809@users.noreply.github.com>
2025-05-28 12:31:11 -0500

    first version

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Home() {
+  return (
+    <main>
+      <Spline scene='https://prod.spline.design/spSrjF5P3IRaQB10/scene.splinecode' />
+    </main>
+  );
+}
